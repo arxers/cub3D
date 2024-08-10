eda38ded6b6d828e646faac9292b7ebb39df27b6
jason_home <arxers@gmail.com>
2024-08-11 01:11:30 +0800

    modularize

--- inc/cub3D.h
+++ inc/cub3D.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3D.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaslim <jaslim@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jaslim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 11:51:12 by jaslim            #+#    #+#             */
-/*   Updated: 2024/08/08 18:24:41 by jaslim           ###   ########.fr       */
+/*   Updated: 2024/08/10 14:40:15 by jaslim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,35 @@
 
 # define RES_X 1280
 # define RES_Y 720
+# define WALL 64
 
 typedef struct s_point
 {
-	int		x;
-	int		y;
-}			t_point;
+	int			x;
+	int			y;
+}				t_point;
 
 typedef struct s_img
 {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_len;
-	int		endian;
-}			t_img;
+	void		*img;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_len;
+	int			endian;
+	t_point		size;
+}				t_img;
 
-typedef struct s_mlx
+typedef struct s_sprites
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_img	img;
-}			t_mlx;
+	t_img		wall[4];
+}				t_sprites;
+
+typedef struct s_game
+{
+	void		*mlx_ptr;
+	void		*win_ptr;
+	t_img		mlx_win_img;
+	t_sprites	sprites;
+}				t_game;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jaslim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 11:44:00 by jaslim            #+#    #+#             */
-/*   Updated: 2024/08/09 01:02:05 by jaslim           ###   ########.fr       */
+/*   Updated: 2024/08/10 14:42:24 by jaslim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,17 +26,74 @@ int	map[10][10] =
 	{1,1,1,1,1,1,1,1,1,1}
 };
 
-void	ft_pixel_put(t_img *img, int x, int y, int color)
+void	set_pixel(t_img *img, int x, int y, unsigned int color)
 {
-	char	*dst;
+	char			*dst;
 
-	if (x < 0 || y < 0 || x > RES_X || y > RES_Y)
+	if (x < 0 || y < 0 || x > RES_X || y > RES_Y || (color >> 24) & 0xFF)
 		return ;
 	dst = img->addr + ((y * img->line_len) + (x * (img->bits_per_pixel / 8)));
 	*(unsigned int *)dst = color;
 }
 
-void	draw_circle(t_img *img, t_point origin, int radius, int color)
+unsigned int	get_pixel(t_img *img, int x, int y)
+{
+	char	*src;
+
+	src = img->addr + ((y * img->line_len) + (x * (img->bits_per_pixel / 8)));
+	return (*(unsigned int *)src);
+}
+
+void	img_to_img(t_point offset, t_point size, t_img *src, t_img *dst)
+{
+	int		x;
+	int		y;
+	y = 0;
+	if (offset.x < 0 || offset.y < 0
+		|| (offset.x + size.x) > (dst->line_len / (dst->bits_per_pixel / 8))
+		|| (offset.y + size.y) > (dst->line_len / (dst->bits_per_pixel / 8)))
+        return;
+	while (y < size.y)
+	{
+		x = 0;
+		while (x < size.x)
+		{
+			set_pixel(dst, x + offset.x, y + offset.y, get_pixel(src, x, y));
+			x++;
+		}
+		y++;
+	}
+}
+
+void	transparency_test(t_game *cub3d)
+{
+	t_img	test_alpha;
+	t_point size;
+
+	test_alpha.img = mlx_xpm_file_to_image(cub3d->mlx_ptr, "textures/alpha_test.xpm", &size.x, &size.y);
+	test_alpha.addr = mlx_get_data_addr(test_alpha.img, &test_alpha.bits_per_pixel,
+			&test_alpha.line_len, &test_alpha.endian);
+	img_to_img((t_point){RES_X / 2 - (size.x / 2), RES_Y / 2 - (size.y / 2)}, size, &test_alpha, &cub3d->mlx_win_img);
+}
+
+int	load_xpm(void *mlx, char *path, t_img *img)
+{
+	img->img = mlx_xpm_file_to_image(mlx, path, &img->size.x, &img->size.y);
+	if (!img->img)
+		return (-1);
+	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_len, &img->endian);
+	return (0);
+}
+
+int	load_xpms(t_game *cub3d)
+{
+	if (load_xpm(cub3d->mlx_ptr, "textures/wall/wall1.xpm", &cub3d->sprites.wall[0]) == -1)
+		return (-1);
+	img_to_img((t_point){RES_X / 2 - (cub3d->sprites.wall[0].size.x / 2), RES_Y / 2 - (cub3d->sprites.wall[0].size.y / 2)}, cub3d->sprites.wall[0].size, &cub3d->sprites.wall[0], &cub3d->mlx_win_img);
+	return (0);
+}
+
+void	draw_circle(t_img *img, t_point origin, int radius, unsigned int color)
 {
 	int		x;
 	int		y;
@@ -47,7 +104,7 @@ void	draw_circle(t_img *img, t_point origin, int radius, int color)
 	{
 		x = origin.x + radius * cos(angle * M_PI / 180);
 		y = origin.y + radius * sin(angle * M_PI / 180);
-		ft_pixel_put(img, x, y, color);
+		set_pixel(img, x, y, color);
 		angle += 0.1;
 	}
 }
@@ -66,22 +123,36 @@ int	input_validation(int ac, char **av)
 	return (0);
 }
 
+int	cleanup(t_game *cub3d)
+{
+	mlx_destroy_image(cub3d->mlx_ptr, cub3d->sprites.wall[0].img);
+	mlx_destroy_image(cub3d->mlx_ptr, cub3d->mlx_win_img.img);
+	mlx_destroy_window(cub3d->mlx_ptr, cub3d->win_ptr);
+	mlx_destroy_display(cub3d->mlx_ptr);
+	free(cub3d->mlx_ptr);
+	exit(0);
+}
+
 int	main(int ac, char **av)
 {
-	t_mlx	cub3d;
+	t_game	cub3d;
 
 	if (input_validation(ac, av) == -1)
 		return (1);
-	cub3d.mlx = mlx_init();
-	cub3d.mlx_win = mlx_new_window(cub3d.mlx, RES_X, RES_Y, "cub3D");
-	cub3d.img.img = mlx_new_image(cub3d.mlx, RES_X, RES_Y);
-	cub3d.img.addr = mlx_get_data_addr(cub3d.img.img, &cub3d.img.bits_per_pixel,
-			&cub3d.img.line_len, &cub3d.img.endian);
+	cub3d.mlx_ptr = mlx_init();
+	cub3d.win_ptr = mlx_new_window(cub3d.mlx_ptr, RES_X, RES_Y, "cub3D");
+	cub3d.mlx_win_img.img = mlx_new_image(cub3d.mlx_ptr, RES_X, RES_Y);
+	cub3d.mlx_win_img.addr = mlx_get_data_addr(cub3d.mlx_win_img.img, &cub3d.mlx_win_img.bits_per_pixel,
+			&cub3d.mlx_win_img.line_len, &cub3d.mlx_win_img.endian);
 	// ft_pixel_put(&cub3d.img, 5, 5, 0x00FF00);
 	// ft_pixel_put(&cub3d.img, 10, 10, 0x00FF00);
-	draw_circle(&cub3d.img, (t_point){.x = (RES_X / 2), .y = (RES_Y / 2)}, 100, 0xFF00FF);
-	mlx_put_image_to_window(cub3d.mlx, cub3d.mlx_win, cub3d.img.img, 0, 0);
-	mlx_loop(cub3d.mlx);
+	// for (int i = 0; i < 500; i += 10)
+	load_xpms(&cub3d);
+	draw_circle(&cub3d.mlx_win_img, (t_point){.x = (RES_X / 2), .y = (RES_Y / 2)}, 400, 0x00FF00);
+	// transparency_test(&cub3d);
+	mlx_put_image_to_window(cub3d.mlx_ptr, cub3d.win_ptr, cub3d.mlx_win_img.img, 0, 0);
+	// cleanup(&cub3d);
+	mlx_loop(cub3d.mlx_ptr);
 	(void)av;
 	return (0);
 }
