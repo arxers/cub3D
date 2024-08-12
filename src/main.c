72ec2f64def45a3d063896332158fdd0a1ffecfc
arxers <arxers@gmail.com>
2024-08-12 18:11:42 +0800

    wip input handling

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaslim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: jaslim <jaslim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 11:44:00 by jaslim            #+#    #+#             */
-/*   Updated: 2024/08/10 14:42:24 by jaslim           ###   ########.fr       */
+/*   Updated: 2024/08/12 18:11:32 by jaslim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	map[10][10] =
 void	set_pixel(t_img *img, int x, int y, unsigned int color)
 {
 	char			*dst;
-
+	
 	if (x < 0 || y < 0 || x > RES_X || y > RES_Y || (color >> 24) & 0xFF)
 		return ;
 	dst = img->addr + ((y * img->line_len) + (x * (img->bits_per_pixel / 8)));
@@ -68,7 +68,7 @@ void	img_to_img(t_point offset, t_point size, t_img *src, t_img *dst)
 void	transparency_test(t_game *cub3d)
 {
 	t_img	test_alpha;
-	t_point size;
+	t_point	size;
 
 	test_alpha.img = mlx_xpm_file_to_image(cub3d->mlx_ptr, "textures/alpha_test.xpm", &size.x, &size.y);
 	test_alpha.addr = mlx_get_data_addr(test_alpha.img, &test_alpha.bits_per_pixel,
@@ -91,6 +91,24 @@ int	load_xpms(t_game *cub3d)
 		return (-1);
 	img_to_img((t_point){RES_X / 2 - (cub3d->sprites.wall[0].size.x / 2), RES_Y / 2 - (cub3d->sprites.wall[0].size.y / 2)}, cub3d->sprites.wall[0].size, &cub3d->sprites.wall[0], &cub3d->mlx_win_img);
 	return (0);
+}
+
+void	draw_rectangle(t_img *img, t_point origin, t_point size, unsigned int color)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < size.y)
+	{
+		x = 0;
+		while (x < size.x)
+		{
+			set_pixel(img, x + origin.x, y + origin.y, color);
+			x++;
+		}
+		y++;
+	}
 }
 
 void	draw_circle(t_img *img, t_point origin, int radius, unsigned int color)
@@ -133,26 +151,110 @@ int	cleanup(t_game *cub3d)
 	exit(0);
 }
 
+t_point	center(t_point origin, t_point size)
+{
+	origin.x -= size.x / 2;
+	origin.y -= size.y / 2;
+	return (origin);
+}
+
+t_img	create_bg(void *mlx_ptr)
+{
+	t_img	bg;
+
+	bg.img = mlx_new_image(mlx_ptr, RES_X, RES_Y);
+	bg.addr = mlx_get_data_addr(bg.img, &bg.bits_per_pixel, &bg.line_len, &bg.endian);
+	draw_rectangle(&bg, (t_point){0, 0}, (t_point){RES_X, RES_Y / 2}, 0x171B22);
+	draw_rectangle(&bg, (t_point){0, RES_Y / 2}, (t_point){RES_X, RES_Y / 2}, 0x404856);
+	return (bg);
+}
+
+// int	handle_movement(unsigned int	key)
+// {
+
+// }
+
+int	handle_esc_button(void)
+{
+	ft_printf("Exiting with close button\n");
+	exit(0);
+	return (0);
+}
+
+int	key_release(unsigned int key, t_game *game)
+{
+	if (key == XK_Up)
+		game->arrow_keys[0] = 0;
+	if (key == XK_Left)
+		game->arrow_keys[1] = 0;
+	if (key == XK_Down)
+		game->arrow_keys[2] = 0;
+	if (key == XK_Right)
+		game->arrow_keys[3] = 0;
+	return (0);
+}
+
+int	key_press(unsigned int key, t_game *game)
+{
+	if (key == XK_Up)
+		game->arrow_keys[0] = 1;
+	if (key == XK_Left)
+		game->arrow_keys[1] = 1;
+	if (key == XK_Down)
+		game->arrow_keys[2] = 1;
+	if (key == XK_Right)
+		game->arrow_keys[3] = 1;
+	return (0);
+}
+
+int	handle_keys(t_game *game)
+{
+	if (game->arrow_keys[0])
+		ft_printf("up\n");
+	if (game->arrow_keys[1])
+		ft_printf("left\n");
+	if (game->arrow_keys[2])
+		ft_printf("down\n");
+	if (game->arrow_keys[3])
+		ft_printf("right\n");
+	return (0);
+}
+
+void	init_keys(t_game *game)
+{
+	int	i;
+
+	i = 0;
+	while (i < 4)
+	{
+		game->arrow_keys[i] = 0;
+		i++;
+	}
+}
+
 int	main(int ac, char **av)
 {
-	t_game	cub3d;
+	t_game	game;
 
 	if (input_validation(ac, av) == -1)
 		return (1);
-	cub3d.mlx_ptr = mlx_init();
-	cub3d.win_ptr = mlx_new_window(cub3d.mlx_ptr, RES_X, RES_Y, "cub3D");
-	cub3d.mlx_win_img.img = mlx_new_image(cub3d.mlx_ptr, RES_X, RES_Y);
-	cub3d.mlx_win_img.addr = mlx_get_data_addr(cub3d.mlx_win_img.img, &cub3d.mlx_win_img.bits_per_pixel,
-			&cub3d.mlx_win_img.line_len, &cub3d.mlx_win_img.endian);
-	// ft_pixel_put(&cub3d.img, 5, 5, 0x00FF00);
-	// ft_pixel_put(&cub3d.img, 10, 10, 0x00FF00);
-	// for (int i = 0; i < 500; i += 10)
-	load_xpms(&cub3d);
-	draw_circle(&cub3d.mlx_win_img, (t_point){.x = (RES_X / 2), .y = (RES_Y / 2)}, 400, 0x00FF00);
-	// transparency_test(&cub3d);
-	mlx_put_image_to_window(cub3d.mlx_ptr, cub3d.win_ptr, cub3d.mlx_win_img.img, 0, 0);
+	game.mlx_ptr = mlx_init();
+	game.win_ptr = mlx_new_window(game.mlx_ptr, RES_X, RES_Y, "cub3D");
+	game.mlx_win_img.img = mlx_new_image(game.mlx_ptr, RES_X, RES_Y);
+	game.mlx_win_img.addr = mlx_get_data_addr(game.mlx_win_img.img, &game.mlx_win_img.bits_per_pixel,
+			&game.mlx_win_img.line_len, &game.mlx_win_img.endian);
+	game.bg = create_bg(game.mlx_ptr);
+	img_to_img((t_point){0, 0}, (t_point){RES_X, RES_Y}, &game.bg, &game.mlx_win_img);
+	transparency_test(&game);
+	mlx_put_image_to_window(game.mlx_ptr, game.win_ptr, game.mlx_win_img.img, 0, 0);
 	// cleanup(&cub3d);
-	mlx_loop(cub3d.mlx_ptr);
+
+	init_keys(&game);
+	mlx_hook(game.win_ptr, KeyPress, KeyPressMask, &key_press, &game);
+	mlx_hook(game.win_ptr, KeyRelease, KeyReleaseMask, &key_release, &game);
+	mlx_hook(game.win_ptr, DestroyNotify, StructureNotifyMask, &handle_esc_button, NULL);
+	mlx_loop_hook(game.mlx_ptr, &handle_keys, &game);
+	mlx_loop(game.mlx_ptr);
 	(void)av;
 	return (0);
 }
