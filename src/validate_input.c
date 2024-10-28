9ede52823382aeabe9e7000075817c3f31c3ecaa
jsu <jsu@student.42singapore.sg>
2024-10-28 16:04:13 +0800

    added free() after ft_split(tmp_map_buf)

--- src/validate_input.c
+++ src/validate_input.c
@@ -730,7 +730,8 @@ int is_rcol_all_walls(char **s)
 }
 
 /*
-check if all 4 sides of rectangular map, consists of only wall chars
+To do
+check if all 4 sides of rectangular map, consists of only 1 or 0, but not F chars
 
 check top row
 check bot row
@@ -753,6 +754,58 @@ int	is_map_surrounded_by_walls(char **s)
 //# DONE above
 //##############################################################################
 //# pending below
+
+
+/*
+returns the count of chars 'ch' found in (char **)map
+*/
+int	count_char_in_map(char **map, char ch)
+{
+	int	i;
+	int	j;
+	int	res;
+	
+	res = 0;
+	i = 0;
+	while (map[i] != NULL)
+	{
+		j = 0;
+		while(map[i][j] != '\0')
+		{
+			if (map[i][j] == ch)
+				res += 1;
+			j++;
+		}
+		i++;
+	}
+	return (res);
+}
+
+/*
+scan the map, and count the number of valid player chars
+if sum total of valid player char is non-zero, return -1 (error)
+else return 0 (success) 
+*/
+int	is_num_player_valid(char **map)
+{
+	int num_N;
+	int num_S;
+	int num_E;
+	int num_W;
+	
+	num_N = count_char_in_map(map, 'N');
+	num_S = count_char_in_map(map, 'S');
+	num_E = count_char_in_map(map, 'E');
+	num_W = count_char_in_map(map, 'W');
+	if (num_N + num_S + num_E + num_W != 1)
+	{
+		ft_putstr_fd("cub3D: Incorrect number of player char in map\n", 2);
+		return (-1);
+	}
+	return (0);
+}
+
+
 
 /*
 copy data from src, (char **)tmp map
@@ -924,20 +977,34 @@ int	load_scene(char *mapfile, t_scene *scene) // TO DO: rename as load_scene
 	replace_space_with_wall(scene->tmp_map_buf); // replace space with zeroes instead
 	if (is_tmp_map_buf_split_by_empty_line(scene->tmp_map_buf) == -1)
 		return (-1);
-	
-	tmp = ft_split(scene->tmp_map_buf, '\n');	
+	tmp = ft_split(scene->tmp_map_buf, '\n');
+	if (!tmp)
+	{
+		ft_putstr_fd("cub3D: Error ft_split tmp_map_buf\n", 2);
+		return (-1);
+	}
 	if (init_map_array(tmp, scene) == -1)
 	{
 		ft_putstr_fd("cub3D: Error in malloc map\n", 2);
 		return (-1);
 	}
-	// copy data from (char **)tmp to (char **)scene->map
-	
 	load_map_data(tmp, scene);
-	print_2d_map(scene->map);
+	free_char_map(tmp);
 	
-	if (is_map_surrounded_by_walls(scene->map) == -1)
+	// count number of player chars, N, S, E, W
+	// is_num_player_valid(), ensure only 1 player char in total
+	if (is_num_player_valid(scene->map) == -1)
 		return (-1);
+
+	
+	// load_player_char_coordinates
+	// flood fill from player char coordinates
+	
+	//if (is_map_surrounded_by_walls(scene->map) == -1)
+	//	return (-1);
+
+	print_2d_map(scene->map); // debug
+	
 		
 	// is map minimum 3 by 3 
 	// is_map_valid(scene>map);
